cf659fb01422ffd347092eeee1d8dcbc17830d29
Sandra Misigo <misigosandra@gmail.com>
2024-11-27 11:18:03 +0300

    myphase0loopingalong

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+
+// Function to create thank-you messages
+function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+  }
+  
+  // Function to count down from a given number to 0
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+  // Export the functions for testing
+  module.exports = {
+    writeCards,
+    countDown,
+  };
